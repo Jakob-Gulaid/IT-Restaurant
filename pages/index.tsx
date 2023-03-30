7195f9b6860b4bef1cec15f3af086b4a8b674666
Jakob-Gulaid <japea002@osloskolen.no>
2023-03-30 10:00:29 +0200

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+export default function Home() {
+
+  return (
+    <div className="flex flex-col text-center ">
+  <button className="btn "
+  >
+    klikk meg
+  </button>
+  </div>
+  )
+}
